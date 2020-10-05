e180a0b532c686fa57db1cc7eed50ea01b46c05b
petem46 <petem46@googlemail.com>
2020-10-05 18:07:55 +0100

    page edit toolbar

--- resources/js/components/EditorBarTextBlock.vue
+++ resources/js/components/EditorBarTextBlock.vue
@@ -1,133 +1,115 @@
 <template>
 	<editor-menu-bar :editor="editor" v-slot="{ commands, getMarkAttrs, isActive }">
-		<v-toolbar dense flat color="grey lighten-3">
-			<v-btn icon text small @click="commands.undo">
-				<v-icon>mdi-undo</v-icon>
-			</v-btn>
-
-			<v-btn icon text small @click="commands.redo">
-				<v-icon>mdi-redo</v-icon>
-			</v-btn>
-
-			<v-divider class="mx-2" inset vertical></v-divider>
-
-			<v-btn icon text small :class="{ 'is-active': isActive.bold() }" @click="commands.bold">
-				<v-icon>mdi-format-bold</v-icon>
-			</v-btn>
-
-			<v-btn icon text small :class="{ 'is-active': isActive.italic() }" @click="commands.italic">
-				<v-icon>mdi-format-italic</v-icon>
-			</v-btn>
-
-			<v-btn
-				icon
-				text
-				small
-				:class="{ 'is-active': isActive.underline() }"
-				@click="commands.underline"
-			>
-				<v-icon>mdi-format-underline</v-icon>
-			</v-btn>
-
-			<v-divider class="mx-2" inset vertical></v-divider>
-
-			<v-btn
-				icon
-				text
-				small
-				class="teal--text px-0"
-				:class="{'is-active':getMarkAttrs('colortext').class == 'teal--text'}"
-				@click="setTealText(commands.colortext, getMarkAttrs('colortext').class)"
-			>
-				<v-icon>mdi-format-color-text</v-icon>
-			</v-btn>
-
-			<v-btn
-				icon
-				text
-				small
-				class="red--text px-0"
-				:class="{'is-active':getMarkAttrs('colortext').class == 'red--text'}"
-				@click="setRedText(commands.colortext, getMarkAttrs('colortext').class)"
-			>
-				<v-icon>mdi-format-color-text</v-icon>
-			</v-btn>
-
-			<v-btn
-				icon
-				text
-				small
-				class="orange--text text-darken-2 px-0"
-				:class="{
+		<v-toolbar prominent flat color="grey lighten-3">
+			<v-row>
+				<v-col cols="12">
+					<v-btn icon text small @click="commands.undo">
+						<v-icon>mdi-undo</v-icon>
+					</v-btn>
+					<v-btn icon text small @click="commands.redo">
+						<v-icon>mdi-redo</v-icon>
+					</v-btn>
+					<v-divider class="mx-2" inset vertical></v-divider>
+					<v-btn icon text small :class="{ 'is-active': isActive.bold() }" @click="commands.bold">
+						<v-icon>mdi-format-bold</v-icon>
+					</v-btn>
+					<v-btn icon text small :class="{ 'is-active': isActive.italic() }" @click="commands.italic">
+						<v-icon>mdi-format-italic</v-icon>
+					</v-btn>
+					<v-btn
+						icon
+						text
+						small
+						:class="{ 'is-active': isActive.underline() }"
+						@click="commands.underline"
+					>
+						<v-icon>mdi-format-underline</v-icon>
+					</v-btn>
+					<v-divider class="mx-2" inset vertical></v-divider>
+					<v-btn
+						icon
+						text
+						small
+						class="teal--text px-0"
+						:class="{'is-active':getMarkAttrs('colortext').class == 'teal--text'}"
+						@click="setTealText(commands.colortext, getMarkAttrs('colortext').class)"
+					>
+						<v-icon>mdi-format-color-text</v-icon>
+					</v-btn>
+					<v-btn
+						icon
+						text
+						small
+						class="red--text px-0"
+						:class="{'is-active':getMarkAttrs('colortext').class == 'red--text'}"
+						@click="setRedText(commands.colortext, getMarkAttrs('colortext').class)"
+					>
+						<v-icon>mdi-format-color-text</v-icon>
+					</v-btn>
+					<v-btn
+						icon
+						text
+						small
+						class="orange--text text-darken-2 px-0"
+						:class="{
           'is-active':getMarkAttrs('colortext').class == 'orange--text text--darken2',
           }"
-				@click="setOrangeText(commands.colortext, getMarkAttrs('colortext').class)"
-			>
-				<v-icon class>mdi-format-color-text</v-icon>
-			</v-btn>
-
-			<v-divider class="mx-2" inset vertical></v-divider>
-
-			<v-btn
-				icon
-				text
-				small
-				@click="commands.link({ href: 'www.google.com'})"
-			>
-				<v-icon class>mdi-link-plus</v-icon>
-			</v-btn>
-
-			<v-divider class="mx-2" inset vertical></v-divider>
-
-			<v-btn
-				icon
-				text
-				small
-        class="px-0"
-				:class="{ 'is-active': isActive.textH({class: 'text-h5'}) }"
-				@click="commands.textH({ class: 'text-h5'})"
-			>
-				<v-icon v-if="!isActive.textH({class: 'text-h5'})">mdi-format-font-size-increase</v-icon>
-				<v-icon v-if="isActive.textH({class: 'text-h5'})">mdi-format-font-size-decrease</v-icon>
-			</v-btn>
-
-			<v-btn
-				text
-				small
-        class="px-0"
-				:class="{ 'is-active': isActive.heading({ level: 2 }) }"
-				@click="commands.heading({ level: 2 })"
-			>Heading 2</v-btn>
-
-			<v-btn
-				text
-				small
-        class="px-0"
-				:class="{ 'is-active': isActive.heading({ level: 3 }) }"
-				@click="commands.heading({ level: 3 })"
-			>Heading 3</v-btn>
-
-			<v-divider class="mx-2" inset vertical></v-divider>
-
-			<v-btn
-				tile
-				text
-				small
-				:class="{ 'is-active': isActive.bullet_list() }"
-				@click="commands.bullet_list"
-			>
-				<v-icon>mdi-format-list-bulleted</v-icon>
-			</v-btn>
-
-			<v-btn
-				tile
-				text
-				small
-				:class="{ 'is-active': isActive.ordered_list() }"
-				@click="commands.ordered_list"
-			>
-				<v-icon>mdi-format-list-numbered</v-icon>
-			</v-btn>
+						@click="setOrangeText(commands.colortext, getMarkAttrs('colortext').class)"
+					>
+						<v-icon class>mdi-format-color-text</v-icon>
+					</v-btn>
+					<v-divider class="mx-2" inset vertical></v-divider>
+          <v-spacer></v-spacer>
+					<v-btn icon text small @click="commands.link({ href: 'www.google.com'})">
+						<v-icon class>mdi-link-plus</v-icon>
+					</v-btn>
+					<v-btn
+						icon
+						text
+						small
+						class="px-0"
+						:class="{ 'is-active': isActive.textH({class: 'text-h5'}) }"
+						@click="commands.textH({ class: 'text-h5'})"
+					>
+						<v-icon v-if="!isActive.textH({class: 'text-h5'})">mdi-format-font-size-increase</v-icon>
+						<v-icon v-if="isActive.textH({class: 'text-h5'})">mdi-format-font-size-decrease</v-icon>
+					</v-btn>
+					<v-divider class="mx-2" inset vertical></v-divider>
+					<v-btn
+						text
+						small
+						class="px-0"
+						:class="{ 'is-active': isActive.heading({ level: 2 }) }"
+						@click="commands.heading({ level: 2 })"
+					>Heading 2</v-btn>
+					<v-btn
+						text
+						small
+						class="px-0"
+						:class="{ 'is-active': isActive.heading({ level: 3 }) }"
+						@click="commands.heading({ level: 3 })"
+					>Heading 3</v-btn>
+					<v-divider class="mx-2" inset vertical></v-divider>
+					<v-btn
+						icon
+						text
+						small
+						:class="{ 'is-active': isActive.bullet_list() }"
+						@click="commands.bullet_list"
+					>
+						<v-icon>mdi-format-list-bulleted</v-icon>
+					</v-btn>
+					<v-btn
+						icon
+						text
+						small
+						:class="{ 'is-active': isActive.ordered_list() }"
+						@click="commands.ordered_list"
+					>
+						<v-icon>mdi-format-list-numbered</v-icon>
+					</v-btn>
+				</v-col>
+			</v-row>
 		</v-toolbar>
 	</editor-menu-bar>
 </template>
