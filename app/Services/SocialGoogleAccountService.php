b13034f827adb61cd0dca2e9b3e39972e06ad774
petem46 <petem46@googlemail.com>
2020-12-09 16:27:44 +0000

    quick commit

--- app/Services/SocialGoogleAccountService.php
+++ app/Services/SocialGoogleAccountService.php
@@ -17,7 +17,7 @@ class SocialGoogleAccountService
     ->first();
 
     // dd(User::getGoogleSchool($providerUser->getEmail()));
-    // dd($providerUser);
+    dd($providerUser);
     if ($account) {
       $user = User::whereEmail($providerUser->getEmail())->first();
       if ($user) {
