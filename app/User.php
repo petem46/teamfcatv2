8b76aae6f4b6b82e9288ab9107a5b0f6a619f535
petem46 <petem46@googlemail.com>
2021-01-18 08:58:29 +0000

    quick commit

--- app/User.php
+++ app/User.php
@@ -37,7 +37,7 @@ class User extends Authenticatable
 
     public static function checkGarstang($email) {
       $domain = explode("@", $email);
-      if($domain[1] != 'Garstangcommunityacdemy') {
+      if($domain[1] != 'Garstangcommunityacademy') {
         return('fail');
       }
     }
