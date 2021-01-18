ea106ffe5b841df7586b67d7c0492f777496a173
petem46 <petem46@googlemail.com>
2021-01-18 08:47:36 +0000

    quick commit

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -88,7 +88,6 @@ class PagesController extends Controller
       ->join('areas', 'areas.id','=','sections.area_id')
       // ->select('pages.*', DB::raw("CONCAT('/p2',areas.link,'/',pages.slug) as link"), 'sections.link as section_link', 'areas.id as area_id', 'areas.tealTitle as area_title')
       ->select('pages.title', 'pages.subtitle', 'pages.slug', DB::raw("CONCAT('/p2',areas.link,'/',pages.slug) as link"))
-      ->where('deleted_at', null)
       ->orderBy('pages.created_at', 'desc')->limit(5)->get(),
     ];
     return $data;
