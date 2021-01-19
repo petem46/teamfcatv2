7d01e5880d765d3eb749cd261e4239d2eff4d2fb
petem46 <petem46@googlemail.com>
2021-01-19 22:50:10 +0000

    quick commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -22,10 +23,26 @@
   <!-- Styles -->
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-2JENXZFEMQ"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-2JENXZFEMQ');
+
+  </script>
+
 </head>
+
 <body>
   <div id="app">
     @yield('content')
-</div>
+  </div>
 </body>
+
 </html>
