fb937c95c9633b57c6df899423364b09f2a1d41a
petem46 <petem46@googlemail.com>
2021-12-08 14:13:52 +0000

    quick commit

--- resources/js/pages/join/Edit.vue
+++ resources/js/pages/join/Edit.vue
@@ -606,7 +606,7 @@ export default {
 					? (this.vacancyDetails.range =
 							this.scaleRange[0] + "-" + this.scaleRange[1])
 					: this.selectedSalary === 3 // Teacher Main Pay Scale
-					? (this.vacancyDetails.range = "M1-M6")
+					? (this.vacancyDetails.range = "M1-U3")
 					: this.selectedSalary === 4 // Upper Pay Scale
 					? (this.vacancyDetails.range = "U1-U3")
 					: this.selectedSalary === 5 // Unqualified Teacher Pay Scale
